e8e8c4043ddf7b1bcde9fd8af774343a25cd4dfd
prokvk <weirdo@prokvk.com>
2017-04-23 11:30:56 +0200

    updates

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-exec /usr/bin/docker run --rm --hostname my-rabbit --name rabbitmq -e RABBITMQ_DEFAULT_VHOST="/" \
- -e RABBITMQ_ERLANG_COOKIE="secret cookie here" -p 5672:5672 -p 15672:15672 -v /usr/local/src/rabbitmq:/var/lib/rabbitmq rabbitmq
+exec /usr/bin/docker run --rm --hostname my-rabbit --name rabbitmq1 -e RABBITMQ_DEFAULT_VHOST="/" \
+ -e RABBITMQ_ERLANG_COOKIE="secret cookie here" -p 5672:5672 -p 15672:15672 -v /usr/local/src/rabbitmq:/var/lib/rabbitmq rabbitmq1
